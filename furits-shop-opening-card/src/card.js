9cad547870772eba0831e5aee6bc48ccb118b466
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-05-31 22:12:58 +0530

    V1

--- furits-shop-opening-card/src/card.js
+++ furits-shop-opening-card/src/card.js
@@ -2,29 +2,32 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
+    // console.log('card');
     return (
       <>
         <div className="card">
           <h3>
-            <i class="fa-solid fa-store"> </i> TASTY
+            <i className="fa-solid fa-store"> </i> TASTY
           </h3>
           <span id="discount">
-            <i class="fa-solid fa-tag"></i> Discount 50%
+            <i className="fa-solid fa-tag"></i> Discount 50%
           </span>
           <p id="invite">invitation for</p>
           <pre>
-               GRAND
-            <wbr />        OPENING
+            GRAND
+            <wbr /> OPENING
           </pre>
+          <p id="dec">Tasty furits shop</p>
           <address>
-            <i class="fa-solid fa-location-dot"></i> 3/693 , Little Mount ,
+            <i className="fa-solid fa-location-dot"></i> 3/693 , Little Mount ,
             Chennai - 600 021.
           </address>
           <p id="date">
-          <i class="fa-solid fa-clock"></i> MON , 05.06.23 <label>[6pm]</label>
+            <i className="fa-solid fa-clock"></i> MON , 05.06.23{" "}
+            <label>[6pm]</label>
           </p>
           <button>
-            <i class="fa-solid fa-cart-shopping"> </i> Order Now
+            <i className="fa-solid fa-cart-shopping"> </i> Get Offer
           </button>
         </div>
       </>
