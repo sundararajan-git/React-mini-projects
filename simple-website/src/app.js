590830b4f5697c0ecd67826389d27862df13f3fd
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-06-02 16:56:10 +0530

    navbar

---
+++ simple-website/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="conatiner">
+          <Navbar />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
