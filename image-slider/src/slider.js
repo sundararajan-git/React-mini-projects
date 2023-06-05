eb6d86ab4e76078bb9c292197da60158781b9bdc
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-06-05 20:01:21 +0530

    V1

--- image-slider/src/slider.js
+++ image-slider/src/slider.js
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
-import "./CSS_certificate.jpg";
+import React, { useEffect, useState } from "react";
+import HTML from './HTML.jpg';
+import CSS from './CSS.jpg';
+import JS1 from './JS1.jpg';
+import JS2 from './JS2.jpg';
+import REACT from './REACT.jpg';
 
 export default function Slider() {
-  const [image, setimage] = useState("image-slidersrcCSS_certificate.jpg");
+  const [image,setimage] = useState([HTML,CSS,JS1,JS2,REACT]);
+  const [num,setnum] = useState(0)
+  const backward =()=>{
+    if(num==0){
+      setnum(4)
+    }
+    else{
+      setnum(num-1)
+    }
+  }
+  const forward = ()=>{
+    if(num==4){
+      setnum(num-4)
+    }
+    else{
+      setnum(num+1)
+    }
+  }
+  useEffect(()=>{
+    setInterval(() => {
+      setnum(num+1)
+      if(num==4){
+        setnum(0)
+      }
+    }, 5000);
+  })
   return (
-    <div>
-      <img src={image}> </img>
+    <>
+    <a onClick={backward}><i className="fa-solid fa-chevron-left fa-2xl"> </i></a>
+    <div className="container">
+      <img src={image[num]}></img>
     </div>
+    <a onClick={forward}><i className="fa-solid fa-chevron-right fa-2xl"></i></a>
+    </>
   );
 }
