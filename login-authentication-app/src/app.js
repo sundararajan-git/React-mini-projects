92cf02b6e1ba6801ffb50a3b5ad3d2d7070765b4
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-06-11 11:52:51 +0530

    css++

--- login-authentication-app/src/app.js
+++ login-authentication-app/src/app.js
@@ -10,68 +10,89 @@ const App = () => {
   const [login, setLogin] = useState(false);
   const clickHandle = () => {
     setvalid(true);
-    if (form.email && form.password === form.confirmPassword) {
-      setLogin(true);
+    if (form.email && form.password && form.password === form.confirmPassword) {
       console.log("hi");
+      setLogin(true);
     }
   };
   const logout = () => {
     setLogin(!login);
   };
   return (
-    <div>
-      {login ? (
-        <div>
-          <p>Login Succesfully</p>
-          <button onClick={logout}>Log out</button>
-        </div>
-      ) : (
-        <form onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="email">Email</label>
-          <br />
-          <input
-            type="text"
-            id="email"
-            placeholder="Email..."
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
-          />
-          <br />
-          {valid ? form.email ? null : <p>Please enter your email...</p> : null}
-          <label htmlFor="password">Password</label>
-          <br />
-          <input
-            type="password"
-            id="password"
-            placeholder="Password..."
-            onChange={(e) => setForm({ ...form, password: e.target.value })}
-          />
-          <br />
-          {valid ? (
-            form.password ? null : (
-              <p>Please enter your password...</p>
-            )
-          ) : null}
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <br />
-          <input
-            type="password"
-            id="confirm-password"
-            placeholder="Confirm-Password..."
-            onChange={(e) =>
-              setForm({ ...form, confirmPassword: e.target.value })
-            }
-          />
-          <br />
-          {valid ? (
-            !form.email || form.password !== form.confirmPassword ? (
-              <p>Please enter your confirmPassword...</p>
-            ) : null
-          ) : null}
-          <button type="submit" onClick={clickHandle}>
-            Login
-          </button>
-        </form>
-      )}
+    <div className="body">
+      <div className="container">
+        {login ? (
+          <div className="loginSuccess">
+            <p>Login Succesfully !</p>
+            <button onClick={logout} id="logout-btn">
+              Log out
+            </button>
+          </div>
+        ) : (
+          <form onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email..."
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+            {valid ? (
+              form.email ? (
+                <pre> </pre>
+              ) : (
+                <pre id="pre">Please enter your email...</pre>
+              )
+            ) : (
+              <pre> </pre>
+            )}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password..."
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+            {valid ? (
+              form.password ? (
+                <pre> </pre>
+              ) : (
+                <pre id="pre">Please enter your password...</pre>
+              )
+            ) : (
+              <pre> </pre>
+            )}
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm-password"
+              placeholder="Confirm-Password..."
+              onChange={(e) =>
+                setForm({ ...form, confirmPassword: e.target.value })
+              }
+            />
+            {valid ? (
+              !form.confirmPassword ||
+              form.password !== form.confirmPassword ? (
+                <pre id="pre">Please enter your confirmPassword...</pre>
+              ) : (
+                <pre> </pre>
+              )
+            ) : (
+              <pre> </pre>
+            )}
+            <hr />
+            <div>
+              <button>Google</button>
+              <button>Facebook</button>
+            </div>
+            <br />
+            <button type="submit" id="login-btn" onClick={clickHandle}>
+              Login
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
